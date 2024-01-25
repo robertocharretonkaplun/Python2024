7555f2e32b5f3130f18f627f86bd01ffdf375f77
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2024-01-25 11:36:32 -0600

    main - Literales
    Los literales son los datos simples que Python puede manejar. Hay dos tipos principales de literales: numéricos y de cadena de caracteres.

---
+++ Python2024/Python2024/Literales.py
@@ -0,0 +1,128 @@
+
+# Literales de cadena
+
+# Literal de cadena con comillas simples
+cadena = 'Hola mundo!'
+
+# Literal de cadena con comillas dobles
+cadena = "Hola mundo!"
+
+# Imprimiendo las cadenas
+print(cadena)
+
+# Resultado:
+# Hola mundo!
+
+
+# Literal de caracteres
+
+# Literal de carácter
+caracter = 'a'
+
+# Imprimiendo el carácter
+print(caracter)
+
+# Resultado:
+# a
+
+
+# Literales numéricos
+
+# Enteros
+
+entero = 1
+
+# Imprimiendo el entero
+print(entero)
+
+# Resultado:
+# 1
+
+# Decimales
+
+decimal = 1.5
+
+# Imprimiendo el decimal
+print(decimal)
+
+# Resultado:
+# 1.5
+
+# Imaginarios
+
+imaginario = 1j
+
+# Imprimiendo el imaginario
+print(imaginario)
+
+# Resultado:
+# 1j
+
+
+# Literales booleanos
+
+# Literal booleano True
+booleano = True
+
+# Literal booleano False
+booleano = False
+
+# Imprimiendo los booleanos
+print(booleano)
+
+# Resultado:
+# True
+
+
+# Colecciones literales
+
+# Listas
+
+lista = [1, 2, 3, 4]
+
+# Imprimiendo la lista
+print(lista)
+
+# Resultado:
+# [1, 2, 3, 4]
+
+# Tuplas
+
+tupla = (1, 2, 3, 4)
+
+# Imprimiendo la tupla
+print(tupla)
+
+# Resultado:
+# (1, 2, 3, 4)
+
+# Diccionarios
+
+diccionario = {"nombre": "Juan", "edad": 30}
+
+# Imprimiendo el diccionario
+print(diccionario)
+
+# Resultado:
+# {'nombre': 'Juan', 'edad': 30}
+
+
+# Literales especiales
+
+# Literal None
+ninguno = None
+
+# Imprimiendo el literal None
+print(ninguno)
+
+# Resultado:
+# None
+
+# Literal Ellipsis
+tres_puntos = "..."
+
+# Imprimiendo el literal Ellipsis
+print(tres_puntos)
+
+# Resultado:
+# ...
